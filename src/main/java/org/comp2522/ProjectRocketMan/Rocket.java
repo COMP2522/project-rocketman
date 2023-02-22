0f79407bacaec2493b5bf00ee7b41c47a10d0c03
Toor <immangat@gmail.com>
2023-02-21 22:02:21 -0800

    Created a skeleton for Rocket Class.

---
+++ src/main/java/org/comp2522/ProjectRocketMan/Rocket.java
@@ -0,0 +1,19 @@
+package org.comp2522.ProjectRocketMan;
+
+public class Rocket {
+
+  String shoot;
+
+  public Rocket(){
+
+  }
+
+
+  public void shoot(){
+    //pew pew
+  }
+
+  public void speed(){
+    //zoooming
+  }
+}
