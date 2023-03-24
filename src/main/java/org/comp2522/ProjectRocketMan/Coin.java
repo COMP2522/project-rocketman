5da1a7aacf280f3cbdbac48a11908fa3d5b2bdc6
Aditya <2adijbp@gmail.com>
2023-03-24 15:34:05 -0700

    Coin class

--- src/main/java/org/comp2522/ProjectRocketMan/Coin.java
+++ src/main/java/org/comp2522/ProjectRocketMan/Coin.java
@@ -1,29 +1,82 @@
 package org.comp2522.ProjectRocketMan;
+import com.mongodb.client.model.mql.MqlNumber;
 import processing.core.PApplet;
 import processing.core.PImage;
+import processing.core.PVector;
 
-public class Coin {
+import java.util.ArrayList;
+
+public class Coin extends Sprite implements Movable, Destroyable, Collidable {
+
   private PImage image;
+
   private float x;
   private float y;
-  private float size;
+  private float speed;
+  private Window window;
 
-  public Coin(PImage image, float x, float y, float size) {
+  public Coin(PVector position, PVector direction,PImage image, float speed) {
+    super(position, direction);
     this.image = image;
     this.x = x;
     this.y = y;
-    this.size = size;
+    this.window = Window.getInstance();
   }
 
   public void update(float speed) {
     x -= speed;
   }
 
-  public float getPosition() {
-    return x;
+  @Override
+  public void setPosition(PVector position) {
+
   }
 
-  public void draw(PApplet parent) {
-    parent.image(image, x, y, size, size);
+  public PVector getPosition() {
+    return position;
+  }
+
+  @Override
+  public void move() {
+    position.add(-1 * speed,0);
+  }
+
+  @Override
+  public float getSpeed() {
+    return 0;
+  }
+
+  @Override
+  public void setSpeed(float speed) {
+
+  }
+
+  @Override
+  public void setDirection(PVector direction) {
+
+  }
+
+  public void draw() {
+    window.image(image, position.x, position.y);
+  }
+
+  @Override
+  public boolean collided(Player player) {
+    return false;
+  }
+
+  @Override
+  public Boolean isDestroyed() {
+    return null;
+  }
+
+  @Override
+  public void destroy() {
+
+  }
+
+  @Override
+  public void onDestroy() {
+
   }
 }
