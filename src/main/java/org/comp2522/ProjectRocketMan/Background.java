fb2c8995410a547a91f8ace4f4c9eee8aed9f587
Aditya <2adijbp@gmail.com>
2023-04-03 18:50:27 -0700

    Deleted unnecessary zapper

--- src/main/java/org/comp2522/ProjectRocketMan/Background.java
+++ src/main/java/org/comp2522/ProjectRocketMan/Background.java
@@ -10,15 +10,9 @@ import java.util.ArrayList;
  */
 public class Background extends Sprite implements Movable {
   private PImage image;
-
-  private float x;
-  private float y;
   private float speed;
   ArrayList<Heart> coins;
-  ArrayList<Zapper> zappers;
-  private Window window;
-
-  private float scrollSpeed = 1.5f;
+  private final Window window;
 
   /**
    * Constructor of Background class.
@@ -33,19 +27,13 @@ public class Background extends Sprite implements Movable {
     this.window = Window.getInstance();
     this.image = image;
     this.speed = speed;
-    this.x = 0;
-    this.y = 0;
     this.coins = new ArrayList<>();
-    this.zappers = new ArrayList<>();
   }
   public Background(float speed, PVector position, PVector direction) {
     super(position,direction);
     this.window = Window.getInstance();
     this.speed = speed;
-    this.x = 0;
-    this.y = 0;
     this.coins = new ArrayList<>();
-    this.zappers = new ArrayList<>();
   }
 
   /**
@@ -101,64 +89,14 @@ public class Background extends Sprite implements Movable {
     this.direction = direction;
   }
 
-  public void setCoinNum(int numCoins) {
-    // TODO: Implement logic to generate `numCoins` coins in random positions on the screen
-  }
-
-  public void setZapperNum(int numZappers) {
-    // TODO: Implement logic to generate `numZappers` zappers in random positions on the screen
-  }
-
-  /**
-   * Updates the game state by moving the background and updating the positions of the coins and zappers.
-   */
-  public void update() {
-    // Move the background
-    x -= speed;
-    if (x <= -image.width) {
-      x += image.width;
-    }
-
-    // Update the positions of the coins and zappers
-    for (Heart coin : coins) {
-      coin.update(speed);
-    }
-    for (Zapper zapper : zappers) {
-//      zapper.update(speed);
-    }
-  }
-
-//  public ArrayList<Float> getCoinPositions() {
-//    ArrayList<Float> positions = new ArrayList<>();
-//    for (Coin coin : coins) {
-//      positions.add(coin.getPosition());
-//    }
-//    return positions;
-//  }
-
-  /**
-   * Returns a list of positions of all the zappers.
-   * @return an ArrayList of type Float containing the positions of all the zappers
-   */
-  public ArrayList<Float> getZapperPositions() {
-    ArrayList<Float> positions = new ArrayList<>();
-    for (Zapper zapper : zappers) {
-//      positions.add(zapper.getPosition());
-    }
-    return positions;
-  }
-
   /**
    * Draws an image on a window with a scrolling effect.
-   *
    * The image is continuously drawn and scrolled across the window.
    */
   public void draw() {
 
     position.y = speed;
     position.x -= speed;
-//    position.y = scrollSpeed * 16;
-
 
     int offsetX = (int) (position.x % image.width - image.width);
     int offsetY = (int) (position.y % image.height - image.height);
@@ -168,24 +106,6 @@ public class Background extends Sprite implements Movable {
       for (int y = offsetY; y < image.height; y += image.height) {
         window.image(image, x, y);
       }
-
     }
   }
-
-
-
-
-  // Draw the background image
-//    window.image(image, position.x, position.y);
-//    if (x < 0) {
-//      parent.image(image, x + image.width, y);
-////    }
-//
-//    // Draw the coins and zappers
-//    for (Coin coin : coins) {
-//      coin.draw(window);
-//    }
-//    for (Zapper zapper : zappers) {
-//      zapper.draw(window);
-//    }
 }
